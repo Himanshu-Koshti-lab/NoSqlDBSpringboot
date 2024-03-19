861eefe4490fc1bcb1cb69f3271af8f9cab2951f
Himanshu Koshti <Himfeedback74@gmail.com>
2024-03-19 10:38:14 +0530

    Initial commit Log level set.

---
+++ src/main/java/com/SpringDataWithNoSQL/SpringNoSQLDB/SpringNoSqldbApplication.java
@@ -0,0 +1,13 @@
+package com.SpringDataWithNoSQL.SpringNoSQLDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoSqldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNoSqldbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringDataWithNoSQL/SpringNoSQLDB/SpringNoSqldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringDataWithNoSQL.SpringNoSQLDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoSqldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
